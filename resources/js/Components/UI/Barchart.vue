eaf0cdb3f7b13ad32e73cd3a7cd6c5717c62656e
Lenenba <bilitikjulesroger@yahoo.fr>
2025-01-12 16:56:52 -0500

    flash messages

--- resources/js/Components/UI/Barchart.vue
+++ resources/js/Components/UI/Barchart.vue
@@ -191,7 +191,7 @@ onBeforeUnmount(() => {
 
 <template>
     <div
-        class="xl:col-span-4 flex flex-col bg-white border border-stone-200 shadow-sm rounded-sm dark:bg-neutral-800 dark:border-neutral-700">
+        class="xl:col-span-4 flex flex-col border-t-4 border-t-violet-600 bg-white border border-stone-200 shadow-sm rounded-sm dark:bg-neutral-800 dark:border-neutral-700">
         <!-- Header -->
         <div
             class="py-3 px-5 flex flex-wrap justify-between items-center gap-2 border-b border-stone-200 dark:border-neutral-700">
