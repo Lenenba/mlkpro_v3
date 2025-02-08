1dda6d005bf6488550ae73ab0b7fd0e54346b27f
Lenenba <bilitikjulesroger@yahoo.fr>
2025-02-08 11:51:31 -0500

    quote email with tailwind

--- resources/views/emails/quotes/send.blade.php
+++ resources/views/emails/quotes/send.blade.php
@@ -5,173 +5,133 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quote Email</title>
     <style>
-        /* Ajout des styles CSS inline-friendly */
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f9;
-            margin: 0;
-            padding: 0;
-        }
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #ffffff;
-            border: 1px solid #ddd;
-            border-radius: 8px;
-        }
-        .header {
-            background-color: #f9fafb;
-            padding: 20px;
-            border-bottom: 1px solid #ddd;
-        }
-        .header h1 {
-            font-size: 20px;
-            margin: 0;
-            color: #333333;
-        }
-        .details {
-            margin: 20px 0;
-            display: flex;
-            justify-content: space-between;
-        }
-        .details div {
-            font-size: 14px;
-            color: #666666;
-        }
-        .quote-summary {
-            background-color: #f9fafb;
-            padding: 10px;
-            border: 1px solid #ddd;
-            border-radius: 8px;
-            margin-bottom: 20px;
-        }
-        .quote-summary p {
-            margin: 5px 0;
-            font-size: 14px;
-        }
-        .products-table {
-            width: 100%;
-            border-collapse: collapse;
-            margin: 20px 0;
-        }
-        .products-table th, .products-table td {
-            padding: 10px;
-            text-align: left;
-            border: 1px solid #ddd;
-            font-size: 14px;
-        }
-        .products-table th {
-            background-color: #f9fafb;
-            font-weight: bold;
-        }
-        .totals {
-            margin-top: 20px;
-        }
-        .totals div {
-            display: flex;
-            justify-content: space-between;
-            padding: 5px 0;
-        }
-        .totals .highlight {
-            font-weight: bold;
-            color: #333333;
-        }
-        .totals .total {
-            font-size: 16px;
-            font-weight: bold;
-            color: #000000;
-        }
+        {{ file_get_contents(public_path('build/assets/app-DKoz_JkU.css')) }}
     </style>
 </head>
-<body>
-    <div class="container">
-        <!-- Header Section -->
-        <div class="header">
-            <h1>Quote For {{ $quote->customer->company_name }}</h1>
+<body id="content" class="lg:ps-16 pt-[59px] lg:pt-0">
+    <div class="grid grid-cols-5 gap-4">
+        <div class="col-span-1"></div>
+        <div class="col-span-3">
+            <div class="p-5 space-y-3 flex flex-col bg-gray-100 border border-gray-100 rounded-sm shadow-sm xl:shadow-none dark:bg-green-800 dark:border-green-700">
+                <div class="flex justify-between items-center mb-4">
+                    <h1 class="text-xl inline-block font-semibold text-gray-800 dark:text-green-100">
+                        Quote For {{ $quote->customer->company_name }}
+                    </h1>
+                </div>
+                <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+                    <div class="col-span-2 space-x-2">
+                        <div class="bg-white rounded-sm p-4 mb-4">
+                            {{ $quote->job_title }}
+                        </div>
+                        <div class="flex flex-row space-x-6">
+                            <div class="lg:col-span-3">
+                                <p>Property address</p>
+                                <div class="text-xs text-gray-600">
+                                    {{ $quote->customer->properties[0]->country }}
+                                </div>
+                                <div class="text-xs text-gray-600">
+                                    {{ $quote->customer->properties[0]->street1 }}
+                                </div>
+                                <div class="text-xs text-gray-600">
+                                    {{ $quote->customer->properties[0]->state }} - {{ $quote->customer->properties[0]->zip }}
+                                </div>
+                            </div>
+                            <div class="lg:col-span-3">
+                                <p>Contact details</p>
+                                <div class="text-xs text-gray-600">
+                                    {{ $quote->customer->first_name }} {{ $quote->customer->last_name }}
+                                </div>
+                                <div class="text-xs text-gray-600">
+                                    {{ $quote->customer->email }}
+                                </div>
+                                <div class="text-xs text-gray-600">
+                                    {{ $quote->customer->phone }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-white p-4">
+                        <p>Quote details</p>
+                        <div class="text-xs text-gray-600 flex justify-between">
+                            <span> Quote :</span>
+                            <span>{{ $quote?->number }} </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="p-5 space-y-3 flex flex-col bg-white border border-gray-100 rounded-sm shadow-sm xl:shadow-none dark:bg-green-800 dark:border-green-700">
+                <div class="overflow-x-auto">
+                    <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                        <thead>
+                            <tr>
+                                <th class="min-w-[450px]">Product/Services</th>
+                                <th>Qty.</th>
+                                <th>Unit cost</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+                            @foreach ($quote->products as $product)
+                                <tr>
+                                    <td class="px-4 py-3">{{ $product->name }}</td>
+                                    <td class="px-4 py-3">{{ $product->pivot->quantity }}</td>
+                                    <td class="px-4 py-3">{{ $product->pivot->price }}</td>
+                                    <td class="px-4 py-3">{{ $product->pivot->total }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="p-5 grid grid-cols-2 gap-4 justify-between bg-white border border-gray-100 rounded-sm shadow-sm xl:shadow-none dark:bg-green-800 dark:border-green-700">
+                <div></div>
+                <div class="border-l border-gray-200 dark:border-neutral-700 rounded-sm p-4">
+                    <div class="py-4 grid grid-cols-2 gap-x-4">
+                        <div class="col-span-1">
+                            <p class="text-sm text-gray-500 dark:text-neutral-500">Subtotal:</p>
+                        </div>
+                        <div class="col-span-1 flex justify-end">
+                            <p class="text-sm text-green-600 dark:text-green-400">$ {{ $quote->subtotal }}</p>
+                        </div>
+                    </div>
+                    @if ($quote->subtotal < $quote->total)
+                        <div class="space-y-2 py-4 border-t border-gray-200 dark:border-neutral-700">
+                            <div class="flex justify-between">
+                                <p class="text-sm text-gray-500 dark:text-neutral-500">TPS (5%) :</p>
+                                <p class="text-sm text-gray-800 dark:text-neutral-200">${{ number_format($quote->subtotal * 0.05, 2) }}</p>
+                            </div>
+                            <div class="flex justify-between">
+                                <p class="text-sm text-gray-500 dark:text-neutral-500">TVQ (9.975%) :</p>
+                                <p class="text-sm text-gray-800 dark:text-neutral-200">${{ number_format($quote->subtotal * 0.09975, 2) }}</p>
+                            </div>
+                            <div class="flex justify-between font-bold">
+                                <p class="text-sm text-gray-800 dark:text-neutral-200">Total taxes :</p>
+                                <p class="text-sm text-gray-800 dark:text-neutral-200">${{ number_format(($quote->subtotal * 0.05) + ($quote->subtotal * 0.09975), 2) }}</p>
+                            </div>
+                        </div>
+                    @endif
+                    <div class="py-4 grid grid-cols-2 gap-x-4 border-t border-gray-200 dark:border-neutral-700">
+                        <div class="col-span-1">
+                            <p class="text-sm text-gray-800 font-bold dark:text-neutral-500">Total amount:</p>
+                        </div>
+                        <div class="flex justify-end">
+                            <p class="text-sm text-gray-800 font-bold dark:text-neutral-200">$ {{ $quote->total }}</p>
+                        </div>
+                    </div>
+                    @if ($quote->initial_deposit > 0)
+                        <div class="py-4 grid grid-cols-2 items-center gap-x-4 border-t border-gray-600 dark:border-neutral-700">
+                            <div class="col-span-1">
+                                <p class="text-sm text-gray-500 dark:text-neutral-500">Required deposit:</p>
+                            </div>
+                            <div class="flex justify-end">
+                                <span class="text-xs text-gray-500 dark:text-neutral-500">(Min: ${{ $quote->initial_deposit }})</span>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+            </div>
         </div>
-
-        <!-- Quote Details -->
-        <div class="details">
-            <div>
-                <p><strong>Property address:</strong></p>
-                <p>{{ $quote->customer->properties[0]->country }}</p>
-                <p>{{ $quote->customer->properties[0]->street1 }}</p>
-                <p>{{ $quote->customer->properties[0]->state }} - {{ $quote->customer->properties[0]->zip }}</p>
-            </div>
-            <div>
-                <p><strong>Contact details:</strong></p>
-                <p>{{ $quote->customer->first_name }} {{ $quote->customer->last_name }}</p>
-                <p>{{ $quote->customer->email }}</p>
-                <p>{{ $quote->customer->phone }}</p>
-            </div>
-        </div>
-
-        <!-- Quote Summary -->
-        <div class="quote-summary">
-            <p><strong>Quote Details:</strong></p>
-            <p>Quote Number: {{ $quote->number }}</p>
-            <p>Rate Opportunity: ⭐⭐⭐⭐</p>
-        </div>
-
-        <!-- Products Table -->
-        <table class="products-table">
-            <thead>
-                <tr>
-                    <th>Product/Services</th>
-                    <th>Qty.</th>
-                    <th>Unit Cost</th>
-                    <th>Total</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($quote->products as $product)
-                <tr>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->pivot->quantity }}</td>
-                    <td>${{ number_format($product->pivot->price, 2) }}</td>
-                    <td>${{ number_format($product->pivot->total, 2) }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <!-- Totals Section -->
-        <div class="totals">
-            <div>
-                <span>Subtotal:</span>
-                <span>${{ number_format($quote->subtotal, 2) }}</span>
-            </div>
-            <div>
-                <span>Discount (%):</span>
-                <span>Add discount</span>
-            </div>
-            <div>
-                <span>TPS (5%):</span>
-                <span>${{ number_format($quote->subtotal * 0.05, 2) }}</span>
-            </div>
-            <div>
-                <span>TVQ (9.975%):</span>
-                <span>${{ number_format($quote->subtotal * 0.09975, 2) }}</span>
-            </div>
-            <div class="highlight">
-                <span>Total Taxes:</span>
-                <span>${{ number_format(($quote->subtotal * 0.05) + ($quote->subtotal * 0.09975), 2) }}</span>
-            </div>
-            <div class="total">
-                <span>Total Amount:</span>
-                <span>${{ number_format($quote->total, 2) }}</span>
-            </div>
-            <div>
-                <span>Required Deposit (15%):</span>
-                <span>${{ number_format($quote->total * 0.15, 2) }}</span>
-            </div>
-        </div>
-        <!-- Footer -->
-        <div class="footer">
-            Thank you for choosing {{ config('app.name') }}!<br>
-            <a href="{{ route('customer.quote.show', $quote->id) }}">View Quote Online</a>
-        </div>
+        <div class="col-span-1"></div>
     </div>
 </body>
 </html>
