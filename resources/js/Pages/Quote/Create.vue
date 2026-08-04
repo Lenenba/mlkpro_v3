b0c7a9527dd419985d906782fe09e7b882a14f7a
Lenenba <bilitikjulesroger@yahoo.fr>
2026-08-04 17:11:43 -0400

    fix: show and remove quote offer packages

--- resources/js/Pages/Quote/Create.vue
+++ resources/js/Pages/Quote/Create.vue
@@ -476,6 +476,7 @@ const submit = () => {
                                 class="inline-flex min-h-[38px] items-center justify-center rounded-sm border border-transparent bg-green-600 px-3 py-2 text-xs font-medium text-white hover:bg-green-700 disabled:opacity-50"
                                 :disabled="!selectedOfferPackageId"
                                 @click="addOfferPackageLine"
+                                data-testid="quote-offer-package-add"
                             >
                                 Ajouter
                             </button>
